4b1d277db48b825041f36d8bf8d9803ee7ab6077
SebGr <SebGr@users.noreply.github.com>
2020-08-13 14:21:26 +0200

    added test

--- tests/fmlwright/core/data_sources/test_parse_floorplans.py
+++ tests/fmlwright/core/data_sources/test_parse_floorplans.py
@@ -15,4 +15,10 @@ def test_parse_floorplan_txt_rectangle():
 
 def test_parse_floorplan_txt_no_rooms_gdf():
     """Test if a situation with no rooms is handled correctly."""
-    pass
+    walls_file = "./tests/test_data/rectangle.txt"
+    walls_gdf, doors_gdf, rooms_gdf, special_gdf = data_sources.parse_floorplan_txt(
+        walls_file
+    )
+
+    assert rooms_gdf.empty
+    assert walls_gdf.shape[0] == 4
